d8db5b23c8928760b6e7e719ae7592fa3c49fa39
Mario <66450569+Mariosay@users.noreply.github.com>
2022-01-06 11:11:39 -0600

    ciclos

---
+++ ciclos.go
@@ -0,0 +1,15 @@
+package main 
+
+import "fmt"
+
+
+func main(){
+
+	for i:=0;i<10; i++ {
+		// fmt.Println("Hellow")
+		fmt.Println(i)
+	}
+
+}
+
+// aquí solo se ocupa el ciclo for por lo tanto no hay while
